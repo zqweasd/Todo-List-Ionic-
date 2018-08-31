aaba8aaba5abd1a8a82dea052ae84114113118bf
Zar <atinhay@gmail.com>
2018-08-31 19:09:34 +0800

    first na commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+  
 @NgModule({
   declarations: [
     MyApp,
@@ -14,8 +14,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],  
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,49 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
+
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage {	
+  taskinput:any = [];
+  key:string = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+  	private storage: Storage) {
+  	this.storage.get('task').then((task) => {
+  		if (task){
+  			this.taskinput = task;	
+  		}
+  	})
 
   }
+
+  saveData(){
+  	this.taskinput.push(this.key);
+  	this.storage.set('task',this.taskinput);
+
+  }
+  
+  deleteData(task){
+  	var index = this.taskinput.indexOf(task, 0);
+  	if (index > -1){
+  		this.taskinput.splice(index, 1);
+  		this.storage.set('task',this.taskinput);
+
+  	}
+  }
+  editData(task){
+  	var index = this.taskinput.indexOf(task, 0);
+  	if (index > -1){
+  		this.taskinput[index] = this.key;	
+  		this.storage.set('task',this.taskinput);
+
+  	}	
+
+  }
+
 
 }
